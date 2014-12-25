c403a796bd0e627e520c0d9e3c259e36a32d51d6
redgreed <logan.demers96@gmail.com>
2014-12-25 13:59:14 -0500

    General Upload
    Attempt 2

---
+++ New Unity Project/Assets/SurvivalAssets/Scripts/Shooting/RaycastShooting.js
@@ -0,0 +1,58 @@
+ #pragma strict
+
+var Effect : Transform;
+var MuzzleFlash : Light;
+var TheDammage = 100;
+var fireOn=1;
+var fireRate=.1;
+var ammoText : GUIText;
+var MaxAmmo=42;
+var ammo=42;
+
+function Start()
+{
+	MuzzleFlash.enabled=false;
+}
+
+function Update ()
+{
+	if (Input.GetMouseButton(0))
+	{
+		if (fireOn==1)
+		{
+			fireOn=0;
+			fire();	
+		}
+	}
+	 else if (Input.GetKeyDown(KeyCode.R))
+	 {
+		ammo=MaxAmmo;
+		// add animation Declartion here
+	}
+	ammoText.text=ammo.ToString()+"/"+MaxAmmo.ToString();
+}
+function flash() 
+{	
+	MuzzleFlash.enabled = true;
+	yield WaitForSeconds(.1);
+	MuzzleFlash.enabled = false;
+}
+function fire()
+{
+	var hit : RaycastHit;
+	var ray : Ray = Camera.main.ScreenPointToRay(Vector3(Screen.width*0.5, Screen.height*0.5, 0));
+	if (ammo>0)
+	{
+		flash();
+		if (Physics.Raycast (ray, hit, 100))
+		{
+			var particleClone = Instantiate(Effect, hit.point, Quaternion.LookRotation(hit.normal));
+			Destroy(particleClone.gameObject, 2);
+			hit.transform.SendMessage("ApplyDammage", TheDammage, SendMessageOptions.DontRequireReceiver);
+			ammo--;
+		}
+	}		
+	yield WaitForSeconds(fireRate);
+	fireOn=1;
+	audio.Play(0);
+}
