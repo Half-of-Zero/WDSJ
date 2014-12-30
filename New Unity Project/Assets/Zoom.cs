41c3774ed80e16201629bc2cebaa1dee45614af9
redgreed <logan.demers96@gmail.com>
2014-12-30 00:07:03 -0500

    Zoom Added
    Zoom Added and secondary gun added

---
+++ New Unity Project/Assets/Zoom.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zoom : MonoBehaviour 
+{
+	public Camera main;
+	public int zoom;
+	GameObject reg;
+	GameObject zoomed;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (1)) 
+		{
+			//reg.SetActive=false;
+			//zoomed.SetActive=true;
+			main.fieldOfView = zoom;
+		} 
+		else if (Input.GetMouseButtonUp (1)) 
+		{
+			//reg.SetActive=true;
+			//zoomed.SetActive=false;
+			main.fieldOfView = 40;
+		}
+	}
+}
