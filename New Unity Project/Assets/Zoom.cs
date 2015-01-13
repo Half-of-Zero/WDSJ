0e9ac0b17a206ad5a9ca19c5388ef359c42b5038
redgreed <logan.demers96@gmail.com>
2015-01-13 10:00:21 -0500

    Work On Guns

--- New Unity Project/Assets/Zoom.cs
+++ New Unity Project/Assets/Zoom.cs
@@ -7,6 +7,7 @@ public class Zoom : MonoBehaviour
 	public int zoom;
 	public GameObject reg;
 	public GameObject zoomed;
+	private bool fire=false;
 	// Use this for initialization
 	void Start () 
 	{
@@ -14,15 +15,23 @@ public class Zoom : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
 	{
-		if (Input.GetMouseButtonDown (1)) 
+		if (Input.GetMouseButtonDown (0))
+		{
+			fire=true;
+		}
+		if (Input.GetMouseButtonUp (0))
+		{
+			fire=false;
+		}
+		if (Input.GetMouseButtonDown (1)&&fire==false) 
 		{
 			reg.SetActive(false);
 			zoomed.SetActive(true);
 			main.fieldOfView = zoom;
 		} 
-		else if (Input.GetMouseButtonUp (1)) 
+		else if (Input.GetMouseButtonUp (1)&&fire==false) 
 		{
 			reg.SetActive(true);
 			zoomed.SetActive(false);
