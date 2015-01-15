c2eb25e2bed7914654694dcc1f6445ed7f4d9f67
redgreed <logan.demers96@gmail.com>
2015-01-14 21:17:27 -0500

    Lighting For Ship

--- New Unity Project/Assets/SurvivalAssets/Scripts/WeaponSwitching.js
+++ New Unity Project/Assets/SurvivalAssets/Scripts/WeaponSwitching.js
@@ -2,7 +2,7 @@
 
 var Weapon01 : GameObject;
 var Weapon02 : GameObject;
-
+var Weapon : int=1;
 function Update () {
 	if (Input.GetKeyDown(KeyCode.Q))
 	{
@@ -12,14 +12,16 @@ function Update () {
 
 function SwapWeapons()
 {
-	if (Weapon01.active == true)
+	if (Weapon==1)
 	{
-		Weapon01.SetActiveRecursively(false);
-		Weapon02.SetActiveRecursively(true);
+		Weapon=2;
+		Weapon01.SetActive(false);
+		Weapon02.SetActive(true);
 	}
 	else 
 	{
-		Weapon01.SetActiveRecursively(true);
-		Weapon02.SetActiveRecursively(false);
+		Weapon=1;
+		Weapon01.SetActive(true);
+		Weapon02.SetActive(false);
 	}
 }
