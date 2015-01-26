7155e744dff3532b9491b4001f229472b3b089e7
Nick Macneil <nickmac132@gmail.com>
2015-01-26 08:40:48 -0500

    puhsing. finally

--- New Unity Project/Assets/Pushing.cs
+++ New Unity Project/Assets/Pushing.cs
@@ -3,23 +3,17 @@ using System.Collections;
 
 public class Pushing : MonoBehaviour {
 	public float pushPower = 2.0f;
-	CharacterController thisController;
-
-	void Awake(){
-		thisController = GetComponent<CharacterController> ();
-	}
 
 	void OnControllerHit(ControllerColliderHit hit){
-		//print ("called");
-		//Rigidbody body = hit.collider.attachedRigidbody;
+		print ("called");
+		Rigidbody body = hit.collider.attachedRigidbody;
 
-		//if (body == null || body.isKinematic)
-		//	return;
-		//if (hit.moveDirection.y < -0.3f)
-		//	return;
-		//Vector3 pushDir = new Vector3 (hit.moveDirection.x, 0, hit.moveDirection.z);
-		//body.velocity = pushDir * pushPower;
-		Destroy (hit.gameObject);
+		if (body == null || body.isKinematic)
+			return;
+		if (hit.moveDirection.y < -0.3f)
+			return;
+		Vector3 pushDir = new Vector3 (hit.moveDirection.x, 0, hit.moveDirection.z);
+		body.velocity = pushDir * pushPower;
 	}
 
 }
